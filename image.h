f6776ec5852eea3cb12aa5adda6d7e69c37523e5
Aakash Yuvraj Ingle <AakashYuvraj.Ingle@philips.com>
2024-12-04 17:41:40 +0530

    required changes to refactor and reduce code

--- image.h
+++ image.h
@@ -5,6 +5,7 @@
 class Image {
  public:
     Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
+		static_assert(rows <= 1024 && columns <= 1024, "Image dimensions are too large");
         pixels = new uint8_t[m_rows * m_columns];
     }
     ~Image() {
@@ -13,6 +14,13 @@ class Image {
     }
     bool imageSizeIsValid() {
         return m_rows <= 1024 && m_columns <= 1024;
+    }
+	void processPixels(const std::function<void(int, int)>& processor) {
+        for (int x = 0; x < m_rows; x++) {
+            for (int y = 0; y < m_columns; y++) {
+                processor(x, y);
+            }
+        }
     }
     const uint16_t m_rows;
     const uint16_t m_columns;
